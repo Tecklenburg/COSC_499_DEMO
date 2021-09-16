5ff8432b1f9a72362928c0fa6cea69243691b20a
“Niklas <2465258T@student.gla.ac.uk>
2021-09-15 20:16:50 -0700

    Initialize Project

---
+++ sorting_algorithms/src/sorting_algorithms/Sorting.java
@@ -0,0 +1,15 @@
+package sorting_algorithms;
+
+public class Sorting{
+	public static void main (String[] args) {
+		System.out.println("Interesting Stuff to come");
+	}
+	
+	public int[] bubbleSort (int[] arr) {
+		return arr;
+	}
+	
+	public int[] insertionSort (int[] arr) {
+		return arr;
+	}
+}
